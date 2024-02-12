7e42638078cd33a21c4bf7d48a534191eea1e0ad
Damian-Zsiros-Smartinfo <practicanteing2@smartinfo.com.co>
2024-02-12 05:43:11 -0500

    feat: add part of functionality for send message for get chat messsage

--- src/services/chatService.ts
+++ src/services/chatService.ts
@@ -8,15 +8,24 @@ export async function getChatMessages() {
       .select('id,name_sender,message')
 
     if (!chat_messages) throw new Error()
-
-    return chat_messages.map((message: any) => {
-      const messageInfo = message as Message
-      return {
-        id: messageInfo.id,
-        actor: messageInfo.name_sender,
-        text: messageInfo.message,
+    const chatMessagesWithImagesPromises = chat_messages.map(
+      async (message: any) => {
+        const messageInfo = message as Message
+        let { data: images_messages, error: error2 } = await supabase
+          .from('image')
+          .select('*')
+          .eq('id_message', messageInfo.id)
+        return {
+          id: messageInfo.id,
+          actor: messageInfo.name_sender,
+          text: messageInfo.message,
+          images: images_messages,
+        }
       }
-    })
+    )
+    const chatMessagesWithImages = Promise.all(chatMessagesWithImagesPromises)
+    console.log(chatMessagesWithImages)
+    return chatMessagesWithImages
   } catch (error) {
     console.error(error)
   }
--- src/types/chatTypes.ts
+++ src/types/chatTypes.ts
@@ -1,6 +1,19 @@
 export interface Message {
-  id: string;
-  message: string;
-  name_sender: string;
-  images?: File[]
+  id: string
+  message: string
+  name_sender: string
+  images?:
+    | Image[]
+    | {
+        file: {
+          name: string
+        }
+        arrayBuffer: Buffer
+      }[]
+}
+
+export interface Image {
+  id: string
+  link_image: string
+  id_message: string
 }
